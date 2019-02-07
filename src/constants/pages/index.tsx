4866e55cdf247f4ea8473039232387e5f5fc467d
Jan Šilhan <silhanu@gmail.com>
2019-02-07 13:48:49 +0100

    Typed Component

--- src/constants/pages/index.tsx
+++ src/constants/pages/index.tsx
@@ -1,22 +1,28 @@
+import { FunctionComponent } from 'react';
 import { Todo, HelloPage } from '@pages';
+import { RouteComponentProps } from '@reach/router';
 
+export interface RouteExtendedProps extends RouteComponentProps {
+  [key: string]: any;
+}
+// FunctionComponent<RouteExtendedProps> | Component<RouteExtendedProps, any>
 export interface IPagesShape {
-    Component: any;
-    path: string;
-    key: string;
+  Component: FunctionComponent<RouteExtendedProps>;
+  path: string;
+  key: string;
 }
 
 const pages = [
-    {
-        Component: Todo,
-        path: '/todo',
-        key: 'todo',
-    },
-    {
-        Component: HelloPage,
-        path: '/',
-        key: 'index',
-    }
+  {
+    Component: Todo,
+    path: '/todo',
+    key: 'todo'
+  },
+  {
+    Component: HelloPage,
+    path: '/',
+    key: 'index'
+  }
 ];
 
 export default pages;
